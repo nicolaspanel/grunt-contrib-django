5561d052e9988e162c6b038c71c96c1d465eda28
Nick White <nickswhite89@gmail.com>
2014-07-25 23:35:01 +0100

    Start to create grunt-django-manage

--- grunt-tasks/django-manage/django-manage.js
+++ grunt-tasks/django-manage/django-manage.js
@@ -13,6 +13,7 @@ module.exports = function (grunt) {
 
     /**
      * Kills a running node spawn command
+     * @private
      */
     function kill () {
         if (command) {
@@ -27,28 +28,22 @@ module.exports = function (grunt) {
      * @param  {Function} done Contains how to say done, async or not
      */
     function runCommand (cmd, done) {
-        var defaultCmd = [
-                'python',
-                'manage.py'
-            ],
-            run;
-
-        defaultCmd.push(cmd);
-        run = defaultCmd.join(' ');
-
         if (process.platform === 'win32') {
             command = spawn(
                 'cmd.exe',
-                ['/s', '/c', '"' + run + '"'],
+                ['/s', '/c', '"' + cmd + '"'],
                 {windowsVerbatimArguments: true}
             );
         } else {
-            command = spawn('/bin/sh', ['-c', run]);
+            command = spawn('/bin/sh', ['-c', cmd]);
         }
 
-        var stdErrOutput = '';
+        command.stdout.on('data', function (data) {
+            grunt.log.write(data);
+        });
+
         command.stderr.on('data', function (data) {
-            stdErrOutput += data;
+            grunt.log.error(data);
         });
 
         command.on('close', function (code) {
@@ -56,9 +51,6 @@ module.exports = function (grunt) {
                 grunt.log.error(
                     'Django command "' + cmd + '" exited with code ' + code
                 );
-                if (stdErrOutput) {
-                    grunt.log.error('stderr was:\n' + stdErrOutput);
-                }
                 grunt.fail.fatal('Django management command failed.');
                 done(false);
             } else {
@@ -68,65 +60,60 @@ module.exports = function (grunt) {
     }
 
     /**
+     * Makes the Django command based on options and data passed from Grunt
+     * @private
+     * @param  {Object} options Base options for the Grunt task
+     * @param  {Object} data    Extra data to be passed by sub-task
+     * @return {String}         The full build Django manage command
+     */
+    function makeCommand (options, data) {
+        var args = [
+                'python',
+                'manage.py'
+            ],
+            settings = '--settings=';
+
+        for (var attr in data) {
+            options[attr] = data[attr];
+        }
+
+        args.push(options.command);
+
+        if (options.args.length > 0) {
+            args.push(options.args.join(' '));
+        }
+
+        settings = settings + options.app + '.settings.' + options.settings;
+        args.push(settings);
+
+        return args.join(' ');
+    }
+
+    /**
+     * TASK: Django manage Grunt task.
+     *
+     * @name django-manage
+     * @memberOf module:DjangoManage
+     */
+    grunt.task.registerMultiTask('django-manage', function () {
+        // Use node lodash merge clean this up also allows for more data to be
+        // used passed through for opstions from dist:{} etc.
+        var options = this.options(),
+            task = this.args[0],
+            data = this.data[task] || {},
+            cmd = makeCommand(options, data),
+            done = options.async ? this.async() : function () {};
+
+        grunt.log.ok('Using command: ' + cmd.yellow);
+        runCommand(cmd, done);
+    });
+
+    /**
      * Sets on exit of task what to do
+     * @private
      */
     process.on('exit', function () {
         kill();
-    });
-
-    /**
-     * TASK: Runs Django sync db.
-     *
-     * @name django-sync-db
-     * @memberOf module:DjangoManage
-     */
-    grunt.task.registerMultiTask('django-sync-db', function () {
-        var ops = this.options(),
-            args = [
-                'syncdb',
-                '--noinput',
-                '--settings=' + ops.app + '.settings.' + ops.settings
-            ],
-            done = ops.async ? this.async() : function () {},
-            cmd;
-
-        cmd = args.join(' ');
-
-        grunt.log.ok(
-            'Django server syncing for app: ' + ops.app + ' with settings: ' +
-                ops.settings.green
-        );
-        grunt.log.ok('Using command: ' + cmd.grey);
-
-        runCommand(cmd, done);
-    });
-
-    /**
-     * TASK: Loads a Django fixture.
-     *
-     * @name django-load-fixture
-     * @memberOf module:DjangoManage
-     */
-    grunt.task.registerMultiTask('django-load-fixture', function () {
-        var ops = this.options(),
-            fixtures = ops.fixtures.join(' '),
-            args = [
-                'loaddata',
-                fixtures,
-                '--settings=' + ops.app + '.settings.' + ops.settings
-            ],
-            done = ops.async ? this.async() : function () {},
-            cmd;
-
-        cmd = args.join(' ');
-
-        grunt.log.ok(
-            'Loading Django fixture for app: ' + ops.app + ' with settings: ' +
-                ops.settings.green
-        );
-        grunt.log.ok('Using command: ' + cmd.grey);
-
-        runCommand(cmd, done);
     });
 
 };
